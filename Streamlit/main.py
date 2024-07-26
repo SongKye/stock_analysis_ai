e73616e764ee98c4600edeab0d6192818603d1be
SongKye <kjsong1117@gmail.com>
2024-07-26 23:52:05 +0900

    Setup Streamlit repos

---
+++ Streamlit/main.py
@@ -0,0 +1,6 @@
+
+# main.py
+import streamlit as st
+
+st.subheader("ShongStudio의 주식 정보 방")
+st.write("hello worlddfdf")
---
+++ setup_batch.bat
@@ -0,0 +1,4 @@
+
+
+python -m venv streamlit_Env
+.\streamlit_Env\Scripts\activate
